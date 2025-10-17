6bce712f57354eac65ba08b03704d1ddebc5c1f5
prathamjoshi-1 <mr.prathamjoshi1@gmail.com>
2025-10-17 11:26:46 -0700

    Updated Rendering project files

--- Projects/Rendering/src/App.jsx
+++ Projects/Rendering/src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [showbtn, setshowbtn] = useState(true);
 
   return (
     <>
+      {showbtn ? (
+        <button>He..Hee....</button>
+      ) : (
+        <button>Chalo Bhaiya Age Badho</button>
+      )}
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,11 +22,11 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setshowbtn(!showbtn)}>count is {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -29,7 +35,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
